149dfe0f879d0d7a06d0e29019fb481e70855bb1
Abdullah <abdullah.iraqihoussaini@ucdconnect.ie>
2025-01-22 15:46:25 +0100

    clean code except css

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Responsive Portfolio Website
 
-## a fully responsive portfolio website to showcase personal projects
+## A fully responsive portfolio website to showcase personal projects
 
-## why use this?
+## Why Use This?
 
 - you want to develop a portfolio website for yourself that's simple and straight to the point
 - you already have a portfolio and are looking for some design inspiration or code snippets (you want to steal some good looking code and that's completely fine feel free to do it)
-- you want to see what projects I'm making (that's like 0.01% of people)
+- you want to see what projects I'm making (that's like 0.01% of people visiting this repository)
 
-## visual stuff (how this thing looks)
+## Visual Overview
 
 ![home](readme%20visuals/01/home.png)
 ![projects](readme%20visuals/01/projects.png)
--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-/*----------matrix rain----------*/
-// Initialising the canvas
+/*
+=========================================================================================================
+Author      : tzolic
+Name        : Responsive Portfolio Website
+Description : A fully responsive portfolio website to showcase personal projects
+=========================================================================================================
+*/
+
+
+
+//MATRIX RAIN
 var canvas = document.querySelector('canvas'),
     ctx = canvas.getContext('2d');
 
-// Setting the width and height of the canvas
+//setting up
+//width and height of canvas
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Setting up the letters
+//letters
 var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
 letters = letters.split('');
 
-// Setting up the columns
+//columns
 var fontSize = 10,
     columns = canvas.width / fontSize;
 
-// Setting up the drops
+//drops
 var drops = [];
 for (var i = 0; i < columns; i++) {
   drops[i] = 1;
 }
 
-// Setting up the draw function
+//draw function
 function draw() {
   ctx.fillStyle = 'rgba(16, 24, 32, .25)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -36,15 +46,12 @@ function draw() {
   }
 }
 
-// Loop the animation
+// loop the animation
 setInterval(draw, 33);
 
 
 
-
-
-/*----------sidemenu----------*/
-
+//SIDEMENU
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
@@ -56,8 +63,7 @@ function closemenu(){
 
 
 
-
-/*---------navbar scroll----------*/
+//NAVBAR SCROLL
 window.addEventListener("scroll", function(){
   var nav = document.querySelector("nav");
   nav.classList.toggle("sticky", window.scrollY > 0);
@@ -65,7 +71,7 @@ window.addEventListener("scroll", function(){
 
 
 
-//contact form
+//CONTACT FORM
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyQwcteQQwRiSffYEiGcxoNileh-SqeMILCYEBJF0AjMnm-D6ufv2nZ3qugtZf_z7pE/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
@@ -91,16 +97,14 @@ form.addEventListener('submit', e => {
 
 
 
-
-
-//test
+//DYNAMIC PROJECT CARDS
 document.addEventListener('DOMContentLoaded', function() {
   const projectsGrid = document.querySelector('#projects-grid');
   const sortButtons = document.querySelectorAll('.sort-btn');
   
   let projects = [];
 
-  // Fetch JSON data
+  //fetch JSON data
   fetch('projects.json')
     .then(response => response.json())
     .then(data => {
@@ -109,24 +113,26 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .catch(error => console.error('Error loading the JSON data:', error));
 
-  // Function to create the HTML for each project card
+  //function to create the HTML for each project card
   function createProjectCard(project) {
     const projectCard = document.createElement('div');
     projectCard.classList.add('project-card');
     projectCard.dataset.date = project.date;
-    // Generate tech stack HTML dynamically
-
+    
+    //generate dynamically
+    // tech stack
     const techStackHTML = project.techStack
     ? project.techStack.map(tech => `<span class="tech-badge">${tech}</span>`).join('')
     : '';
-    // Generate project links conditionally
+      //project description
+      const descriptionHTML = project.description && project.description.length > 0
+      ? project.description.map(point => `<li>${point}</li>`).join('')
+      : '';
+    //project links
     const liveDemoLink = project.liveDemo ? `<a href="${project.liveDemo}" class="btn">Live Demo</a>` : '';
     const githubLink = project.github ? `<a href="${project.github}" class="btn">GitHub</a>` : '';
-    // Check if project.description exists and is not empty
-    const descriptionHTML = project.description && project.description.length > 0
-    ? project.description.map(point => `<li>${point}</li>`).join('')
-    : '';
-
+  
+    // project card
     projectCard.innerHTML = `
       <div class="project-image">
         <img src="${project.image}" alt="${project.title}">
@@ -149,16 +155,17 @@ document.addEventListener('DOMContentLoaded', function() {
     return projectCard;
   }
 
-  // Function to render all projects
+  //function to render all projects
   function renderProjects(projectsToRender) {
-    projectsGrid.innerHTML = ''; // Clear current content
+    //clear current content
+    projectsGrid.innerHTML = '';
     projectsToRender.forEach(project => {
       const projectCard = createProjectCard(project);
       projectsGrid.appendChild(projectCard);
     });
   }
 
-  // Sort function
+  //sort function
   function convertDateToTimestamp(dateStr) {
     const [day, month, year] = dateStr.split('/');
     return new Date(year, month - 1, day).getTime();
